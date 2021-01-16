db98c040b46f28cebfe43ba203e690ba2c3177cc
karlelik <karlelik@inbox.ru>
2021-01-16 11:44:49 +0700

    test_abs_project added

---
+++ test_abs_project.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number" 
+
+if __name__ == "__main__":
+    test_abs1()
+    print("All tests passed!")
+
